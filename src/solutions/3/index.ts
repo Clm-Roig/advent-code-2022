2bd66f5300d5d545e3c6d7ac0f4e8ca6060c9689
Clément <clm.roig@gmail.com>
2022-12-03 11:01:15 +0100

    refactor & clean day 3 solution

--- src/solutions/3/index.ts
+++ src/solutions/3/index.ts
@@ -25,9 +25,11 @@ const getCharPriority = (char: String): number => {
 
 // Part 2 algo
 function getBadges(dataArray: string[]) {
-  let badges = [];
+  const badges = [];
   let i = 0;
   let groupIdx = 0;
+
+  // Construct all group sacks
   const allSacks: string[][] = [];
   while (i < dataArray.length) {
     const threeSacks = dataArray.slice(i, i + 3);
@@ -35,14 +37,16 @@ function getBadges(dataArray: string[]) {
     i += 3;
     groupIdx += 1;
   }
+
+  // For each group 3 sacks, find the badge
   for (const groupSacks of allSacks) {
     const [sack1, sack2, sack3] = groupSacks;
-    let commonChars = Array.from(new Set(sack1.split(""))).toString(); // remove duplicated chars
-    for (const char of commonChars) {
-      if (!sack2.includes(char) || !sack3.includes(char)) {
-        commonChars = commonChars.replace(char, "");
-      }
-    }
+
+    // Remove duplicated chars (using Set) & iterate to eliminate items not in the 3 sacks
+    let commonChars = Array.from(new Set(sack1.split(""))).filter(
+      (char) => !sack2.includes(char) || !sack3.includes(char)
+    );
+
     if (commonChars.length !== 1) {
       errorMessage = "Part 2: more than 1 common char between sacks";
     } else {
@@ -53,54 +57,35 @@ function getBadges(dataArray: string[]) {
 }
 
 // Part 1 algo
-function getItemAppearingInBothComparment(
-  compartment1: string,
-  compartment2: string
-) {
-  let appearsInBoth = null;
-  let i = 0;
-  while (appearsInBoth === null && i !== compartment1.length) {
-    const item1 = compartment1[i];
-    let j = 0;
-    while (appearsInBoth === null && j !== compartment2.length) {
-      const item2 = compartment2[j];
-      if (item1 === item2) appearsInBoth = item1;
-      j++;
-    }
-    i++;
+function getAllItemsInBothCompartment(dataArray: string[]) {
+  let itemsInBoth = [];
+  for (const rucksack of dataArray) {
+    const middle = rucksack.length / 2;
+    const compartment1 = rucksack.slice(0, middle).split("");
+    const compartment2 = rucksack.slice(middle, rucksack.length).split("");
+    let itemInBothCompartment = compartment1.find((char1) => {
+      return compartment2.includes(char1);
+    });
+    if (itemInBothCompartment) itemsInBoth.push(itemInBothCompartment);
   }
-  return appearsInBoth;
+  return itemsInBoth;
 }
 
 module.exports = async function solution(res: Response, useTestData: boolean) {
   parseFile(useTestData, __dirname, (dataArray) => {
-    let firstPartSolution, secondPartSolution;
-    let itemsInBoth = [];
-
     // Part 1
-    for (const rucksack of dataArray) {
-      const middle = rucksack.length / 2;
-      const compartment1 = rucksack.slice(0, middle);
-      const compartment2 = rucksack.slice(middle, rucksack.length);
-      let itemInBothCompartment = getItemAppearingInBothComparment(
-        compartment1,
-        compartment2
-      );
-      if (itemInBothCompartment) itemsInBoth.push(itemInBothCompartment);
-    }
+    const itemsInBoth = getAllItemsInBothCompartment(dataArray);
 
     // Part 2
-    let badges = getBadges(dataArray);
+    const badges = getBadges(dataArray);
 
     // Format solutions, render view
-    firstPartSolution = itemsInBoth.reduce(
-      (acc, char) => acc + getCharPriority(char),
-      0
-    );
-    secondPartSolution = badges.reduce(
-      (acc, char) => acc + getCharPriority(char),
-      0
-    );
+    const getTotal = (items: string[]) =>
+      items.reduce((acc, char) => acc + getCharPriority(char), 0);
+
+    const firstPartSolution = getTotal(itemsInBoth);
+    const secondPartSolution = getTotal(badges);
+
     res.render("solution", {
       availableSolutions: getAvailableSolutions(),
       dayNb: 3,
