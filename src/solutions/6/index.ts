13cca5addcd620e062570684008745bbe73c7d79
Clément <clm.roig@gmail.com>
2022-12-06 10:04:04 +0100

    day 6 solution

--- src/solutions/6/index.ts
+++ src/solutions/6/index.ts
@@ -1,40 +1,38 @@
 import { Response } from "express";
-import { basename } from "path";
+import { basename, resolve } from "path";
 import getAvailableSolutions from "../../getAvailableSolutions";
 import { parseFile } from "../utils";
 
 let errorMessage: string;
 
-const parseData = (dataArray: string[]) => {
-  for (const line of dataArray) {
-    for (const char of line) {
-    }
+function getStartCharPosition(text: string, nbOfDistinctChars: number): number {
+  let currentChars = "";
+  let found = false;
+  let i = 0;
+  while (!found && i < text.length) {
+    currentChars += text[i];
+    currentChars = currentChars.slice(-nbOfDistinctChars);
+    found =
+      currentChars.length === nbOfDistinctChars &&
+      new Set(currentChars).size === currentChars.length;
+    if (!found) i++;
   }
-};
+  return i + 1;
+}
 
 // Part 1 algo
-function getSolution1(): string {
-  return "sol1";
-}
+const getSolution1 = (text: string): number => getStartCharPosition(text, 4);
 
 // Part 2 algo
-function getSolution2(): string {
-  return "sol2";
-}
+const getSolution2 = (text: string): number => getStartCharPosition(text, 14);
 
 module.exports = async function solution(res: Response) {
   parseFile(__dirname, (testDataArray, dataArray) => {
-    // Parse data
-    parseData(dataArray);
-    parseData(testDataArray);
-
     // Compute solutions
-    // const sol1 = getSolution1(stacks, moves);
-    // const sol2 = getSolution2(stacks, moves);
-    const sol1 = "";
-    const sol2 = "";
-    const testSol1 = getSolution1();
-    const testSol2 = getSolution2();
+    const sol1 = getSolution1(dataArray[0]);
+    const sol2 = getSolution2(dataArray[0]);
+    const testSol1 = getSolution1(testDataArray[0]);
+    const testSol2 = getSolution2(testDataArray[0]);
 
     // Render view
     res.render("solution", {
