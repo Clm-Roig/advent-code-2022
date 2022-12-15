e644c691e676eee44dd2c36b05b7852fb0e916b4
Clément <clm.roig@gmail.com>
2022-12-15 10:20:06 +0100

    day 14 solution

--- src/solutions/14/index.ts
+++ src/solutions/14/index.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { basename } from "path";
 import getAvailableSolutions from "../../getAvailableSolutions";
-import { parseFiles } from "../utils";
+import { parseFile, parseFiles } from "../utils";
 
 let errorMessage: string;
 const ROCK = "＠";
@@ -27,7 +27,7 @@ const parseData = (dataArray: string[]): Cave => {
   let minY = 0;
   let maxX = 0;
   let maxY = 0;
-  // Get min & max x & y
+  // Get min & max x & y to offset all the data
   for (const line of dataArray) {
     const splittedLine = line.split(" -> ").join().split(",");
     const allX = splittedLine
@@ -44,6 +44,8 @@ const parseData = (dataArray: string[]): Cave => {
     .fill(AIR)
     .map(() => Array(maxY - minY + 1).fill(AIR));
 
+  console.table({ minX: minX, maxX: maxX, minY: minY, maxY: maxY });
+
   // Place sand source
   const source = { x: 500 - minX, y: 0 };
   map[500 - minX].pop();
@@ -52,14 +54,15 @@ const parseData = (dataArray: string[]): Cave => {
   // Fill the cave
   for (const line of dataArray) {
     const lineCoords = line.split(" -> ");
-    let prevCord: number[] = [];
+    let prevCoord: number[] = [];
     for (const linePoint of lineCoords) {
       const coords = linePoint.split(",");
       const x = Number(coords[0]) - minX;
       const y = Number(coords[1]) - minY;
-      if (prevCord.length > 0) {
-        const prevX = Number(prevCord[0]);
-        const prevY = Number(prevCord[1]);
+      if (prevCoord.length > 0) {
+        const prevX = Number(prevCoord[0]);
+        const prevY = Number(prevCoord[1]);
+        // Build rock lines from previous rock coord
         for (let i = min(x, prevX); i <= max(x, prevX); i++) {
           map[i][y] = ROCK;
         }
@@ -69,7 +72,7 @@ const parseData = (dataArray: string[]): Cave => {
       } else {
         map[x][y] = ROCK;
       }
-      prevCord = [x, y];
+      prevCoord = [x, y];
     }
   }
 
@@ -95,22 +98,30 @@ const canStop = (map: Cave["map"], x: number, y: number): boolean =>
   isOccupied(map[x - 1][y + 1]);
 
 function getSandDestination(
-  map: Cave["map"],
-  caveHeight: Cave["height"],
-  caveWidth: Cave["width"],
+  cave: Cave,
   x: number,
   y: number
 ): { x: number; y: number } {
-  if (y > caveHeight || x === 0 || x === caveWidth - 1)
+  const { height, map, width } = cave;
+  // Out of bound
+  if (y > height || x === 0 || x === width - 1)
     return { x: Infinity, y: Infinity };
+
+  // Can't fall more: stop here
   if (canStop(map, x, y)) return { x, y };
-  if (!isOccupied(map[x][y + 1]))
-    return getSandDestination(map, caveHeight, caveWidth, x, y + 1);
+
+  // Fall below
+  if (!isOccupied(map[x][y + 1])) return getSandDestination(cave, x, y + 1);
+
+  // Fall to left diagonal
   if (x > 0 && !isOccupied(map[x - 1][y + 1]))
-    return getSandDestination(map, caveHeight, caveWidth, x - 1, y + 1);
+    return getSandDestination(cave, x - 1, y + 1);
+
+  // Fall to right diagonal
   if (!isOccupied(map[x + 1][y + 1]))
-    return getSandDestination(map, caveHeight, caveWidth, x + 1, y + 1);
-  return getSandDestination(map, caveHeight, caveWidth, x, y + 1);
+    return getSandDestination(cave, x + 1, y + 1);
+
+  return { x: Infinity, y: Infinity };
 }
 
 function sandFall(
@@ -123,9 +134,7 @@ function sandFall(
   let stop = false;
   while (!stop) {
     const { x: sandX, y: sandY } = getSandDestination(
-      mapCopy,
-      cave.height,
-      cave.width,
+      { ...cave, map: mapCopy },
       x,
       y
     );
@@ -140,42 +149,38 @@ function sandFall(
   return { map: mapCopy, sandCount };
 }
 
-// Part 1 algo
-function getSolution1(cave: Cave): number {
+// Same algo for both solutions
+function getSolution(cave: Cave): number {
   const {
     source: { x: sourceX, y: sourceY },
   } = cave;
-  const { map: newMap, sandCount } = sandFall(sourceX, sourceY + 1, cave);
+  const { map: newMap, sandCount } = sandFall(sourceX, sourceY, cave);
   printCave(cave);
-
   return sandCount;
-}
-
-// Part 2 algo
-function getSolution2(cave: Cave): number {
-  return -1;
 }
 
 module.exports = async function solution(res: Response) {
   parseFiles(__dirname, (testDataArray, dataArray) => {
-    // Parse data
+    // For Part 2, I added the bottom rock line to the input files, after reading the minX, maxX, minY & maxY values in parseData.
+    parseFile(__dirname, "test-input-2.txt", (testData2Array) => {
+      parseFile(__dirname, "input-2.txt", (data2Array) => {
+        // Compute solutions
+        const sol1 = getSolution(parseData(dataArray));
+        const sol2 = getSolution(parseData(data2Array));
+        const testSol1 = getSolution(parseData(testDataArray));
+        const testSol2 = getSolution(parseData(testData2Array));
 
-    // Compute solutions
-    const sol1 = getSolution1(parseData(dataArray));
-    // const sol2 = getSolution2(data);
-    const sol2 = "";
-    const testSol1 = getSolution1(parseData(testDataArray));
-    const testSol2 = "";
-
-    // Render view
-    res.render("solution", {
-      availableSolutions: getAvailableSolutions(),
-      dayNb: basename(__dirname),
-      errorMessage,
-      testSol1,
-      testSol2,
-      sol1,
-      sol2,
+        // Render view
+        res.render("solution", {
+          availableSolutions: getAvailableSolutions(),
+          dayNb: basename(__dirname),
+          errorMessage,
+          testSol1,
+          testSol2,
+          sol1,
+          sol2,
+        });
+      });
     });
   });
 };
